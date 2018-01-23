df95dee0716dd8166bb49adf7e3100c92a76f42f
Ken <ken.lapolla@gmail.com>
2018-01-23 13:47:00 -0500

    Variables and JS

---
+++ 01_height_to_centimeters.js
@@ -0,0 +1,20 @@
+console.log(72*2.54);
+
+// Variables 1
+var a = 72;
+console.log(a * 2.54);
+
+//Variables 2a
+
+var x = "Aaron is dangerous"
+console.log(x)
+x = "Aaron is more dangerous than juggling chainsaws....on fire!";
+console.log(x)
+
+//Variables 2b
+var theAnswer = 42; //Integer
+console.log(theAnswer);
+theAnswer = "Abe Lincoln"
+console.log(theAnswer);
+
+// Variables 3
